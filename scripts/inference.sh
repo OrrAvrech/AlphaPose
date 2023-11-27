6b896e086c2c87e6d7699f77030d419d5c0c6948
Orr Avrech <orravrech@gmail.com>
2023-11-27 15:37:03 -0500

    update inference script to run on specific files list

--- scripts/inference.sh
+++ scripts/inference.sh
@@ -4,10 +4,11 @@ CONFIG=$1
 CKPT=$2
 VIDEO=$3
 OUTDIR=${4:-"./examples/res"}
+ROOTDIR=${5:-"/proj/vondrick2/orr/AlphaPose"}
 
-python scripts/demo_inference.py \
+python ${ROOTDIR}/scripts/demo_inference.py \
     --cfg ${CONFIG} \
     --checkpoint ${CKPT} \
     --video ${VIDEO} \
     --outdir ${OUTDIR} \
-    --detector yolo --only_pose --save_video
+    --detector yolo --only_pose --save_video --save_img
--- scripts/run_inference_dir.py
+++ scripts/run_inference_dir.py
@@ -7,20 +7,31 @@ from dataclasses import dataclass
 
 @dataclass
 class InferConfig:
+    root_dir: Path
     cfg_path: Path
     ckpt: Path
     output_dir: Path
     vid_dir: Path
-    filenames: Optional[list[Path]]
+    filenames: list
+
+    def __post_init__(self):
+        self.cfg_path = self.root_dir / self.cfg_path
+        self.ckpt = self.root_dir / self.ckpt
 
 
 @pyrallis.wrap()
 def main(cfg: InferConfig):
-    for i, vid_path in enumerate(cfg.vid_dir.rglob("*.mp4")):
+    vid_dir = cfg.vid_dir
+    cfg.output_dir.mkdir(exist_ok=True, parents=True)
+    files = vid_dir.rglob("*.mp4")
+    if len(cfg.filenames) > 0:
+        files = [vid_dir / name for name in cfg.filenames]
+
+    for i, vid_path in enumerate(files):
         vid_output_dir = cfg.output_dir / vid_path.stem
         vid_output_dir.mkdir(exist_ok=True)
         subprocess.run(
-            f"./scripts/inference.sh {cfg.cfg_path} {cfg.ckpt} {vid_path} {vid_output_dir}",
+            f"{cfg.root_dir}/scripts/inference.sh {cfg.cfg_path} {cfg.ckpt} {vid_path} {vid_output_dir}",
             shell=True,
         )
 
